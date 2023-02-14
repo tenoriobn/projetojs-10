48eacfc0c5afbfea7cb5598a0f8939c2ef84d94c
Bruno Tenorio <tenoriobn@gmail.com>
2023-02-14 17:00:53 -0300

    Adicionando condição ao código

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,10 +2,10 @@ const accordionCard = document.querySelectorAll(".accordion__card");
 
 accordionCard.forEach((accordion) => {
     accordion.addEventListener("click", () => {
-        // const accordionAtivo = document.querySelector(".active");
-        // if (accordionAtivo && accordionAtivo !== accordionCard) {
-        //     accordionAtivo.classList.toggle("active");  
-        // }
+        const accordionAtivo = document.querySelector(".active");
+        if (accordionAtivo && accordionAtivo !== accordionCard) {
+            accordionAtivo.classList.toggle("active");  
+        }
         
         accordion.classList.toggle("active");
     });
