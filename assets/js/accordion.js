eda25e05c82a02e71cd1055a4db281dd5e25b8e9
Bruno Tenorio <tenoriobn@gmail.com>
2023-02-18 11:13:42 -0300

    Corrigindo bug de expandir/recolher

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -3,10 +3,11 @@ const accordionCard = document.querySelectorAll(".accordion__card");
 accordionCard.forEach((accordion) => {
     accordion.addEventListener("click", () => {
         const accordionAtivo = document.querySelector(".active");
-        if (accordionAtivo && accordionAtivo !== accordionCard) {
+        if (accordionAtivo && accordionAtivo !== accordion) {
             accordionAtivo.classList.toggle("active");  
         }
         
         accordion.classList.toggle("active");
+
     });
 })
